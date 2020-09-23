ae778cf9cb316b17f639b6edb2b4988d27e5b4fd
SSKGo <54466390+SSKGo@users.noreply.github.com>
2020-09-23 18:38:10 +0900

    Refactor: Remove space for code building

--- src/PerformanceCurvePanel.tsx
+++ src/PerformanceCurvePanel.tsx
@@ -9,7 +9,7 @@ import { GraphLegend, LegendDisplayMode, LegendItem } from '@grafana/ui';
 import { PanelProps } from '@grafana/data';
 import { Field, FieldType, getFieldDisplayName } from '@grafana/data';
 
-interface Props extends PanelProps<CanvasOptions> { }
+interface Props extends PanelProps<CanvasOptions> {}
 
 export const PerformanceCurvePanel: React.FC<Props> = ({ options, data, width, height }) => {
   const styles = getStyles();
